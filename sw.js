e73d8374f41d14909e646f8df745239b0a4c1e6d
ClamDigga76 <jeremywwood76@gmail.com>
2026-01-13 08:13:44 -0500

    Shellfish_Tracker_ESM-007M_RC1FIX3

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Simple offline cache for Shellfish Tracker 
-const CACHE_NAME = "shellfish-tracker-ESM-007L"+self.registration.scope+"-v8";
+const CACHE_NAME = "shellfish-tracker-ESM-007M"+self.registration.scope+"-v8";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./js/app_007L.js",
-  "./js/utils_007L.js",
+  "./js/app_007M.js",
+  "./js/utils_007M.js",
   "./js/utils_007H1B.js",
   "./icons/favicon-32.png",
   "./icons/icon-192.png",
