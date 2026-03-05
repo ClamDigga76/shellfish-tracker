a06287e3abddaf171be33bc69ca29616447c7775
ClamDigga76 <jeremywwood76@gmail.com>
2026-03-05 17:50:01 -0500

    Safer restore flow + bump

--- js/bootstrap_v5.js
+++ js/bootstrap_v5.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "150";
+const SW_VERSION = "151";
 
 // Single source of truth for build/version
 window.APP_BUILD = `v5.${SW_VERSION}`;
@@ -394,3 +394,4 @@ window.addEventListener("error", (ev) => {
 window.addEventListener("unhandledrejection", (ev) => {
   if (window.__showModuleError) window.__showModuleError(ev?.reason || ev);
 });
+
