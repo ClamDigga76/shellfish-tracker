040670c3257805e2eece871342503061fc097fee
ClamDigga76 <jeremywwood76@gmail.com>
2026-04-02 06:55:18 -0400

    refactor settings helper cluster into shared utils module

--- js/settings_screen_v5.js
+++ js/settings_screen_v5.js
@@ -1,13 +1,5 @@
 import { createSettingsSupportRecoverySeam } from "./settings_support_recovery_v5.js";
-
-function escapeSettingsHtml(value) {
-  return String(value ?? "")
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;");
-}
+import { escapeSettingsHtml } from "./settings_utils_v5.js";
 
 export function createSettingsScreenOrchestrator({
   getState,
