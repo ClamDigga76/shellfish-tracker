f7d55cd91d95cfd7b3de4e3078ab03daff788266
ClamDigga76 <jeremywwood76@gmail.com>
2026-04-14 12:17:21 -0400

    Revert "PL-2: Stabilize Home + Reports detail presentation contract"

--- js/reports_screen_v5.js
+++ js/reports_screen_v5.js
@@ -107,41 +107,6 @@ export function createReportsScreenRenderer(deps){
   const queueReportsFocusIntent = reportsTransitionSeam.queueReportsFocusIntent;
   const applyReportsFocusIntent = reportsTransitionSeam.applyReportsFocusIntent;
   const runReportsTransition = reportsTransitionSeam.runReportsTransition;
-  const resolveMetricDetailClassMap = (isHomeMetricDetail)=> isHomeMetricDetail
-    ? {
-      detailSurfaceClass: "homeMetricDetail",
-      detailCardClass: "homeMetricDetailCard",
-      detailBackClass: "homeMetricBackBtn",
-      detailEyebrowClass: "homeMetricEyebrow",
-      detailTitleClass: "homeMetricTitle",
-      detailContextClass: "homeMetricContext",
-      detailHeroWrapClass: "homeMetricHeroWrap",
-      detailHeroLabelClass: "homeMetricHeroLabel",
-      detailHeroValueClass: "homeMetricHeroValue",
-      detailCompareClass: "homeMetricCompare",
-      detailCompareTextClass: "homeMetricCompareText",
-      detailCompareRowsClass: "homeMetricCompareRows",
-      detailChartClass: "homeMetricChartBlock",
-      detailChartContextClass: "homeMetricChartContext",
-      detailInsightClass: "homeMetricInsight"
-    }
-    : {
-      detailSurfaceClass: "reportsMetricDetail",
-      detailCardClass: "reportsMetricDetailCard",
-      detailBackClass: "reportsMetricBackBtn",
-      detailEyebrowClass: "reportsMetricEyebrow",
-      detailTitleClass: "reportsMetricTitle",
-      detailContextClass: "reportsMetricContext",
-      detailHeroWrapClass: "reportsMetricHeroWrap",
-      detailHeroLabelClass: "reportsMetricHeroLabel",
-      detailHeroValueClass: "reportsMetricHeroValue",
-      detailCompareClass: "reportsMetricCompare",
-      detailCompareTextClass: "reportsMetricCompareText",
-      detailCompareRowsClass: "reportsMetricCompareRows",
-      detailChartClass: "reportsMetricChartBlock",
-      detailChartContextClass: "reportsMetricChartContext",
-      detailInsightClass: "reportsMetricInsight"
-    };
 
 function renderReportsScreen({ homeMetricOnly = false } = {}){
   const state = getState();
@@ -202,7 +167,21 @@ function renderReportsScreen({ homeMetricOnly = false } = {}){
     areas: state.areas
   });
 
-  const metricDetailClassMap = resolveMetricDetailClassMap(isHomeMetricDetail);
+  const detailSurfaceClass = isHomeMetricDetail ? "homeMetricDetail" : "reportsMetricDetail";
+  const detailCardClass = isHomeMetricDetail ? "homeMetricDetailCard" : "reportsMetricDetailCard";
+  const detailBackClass = isHomeMetricDetail ? "homeMetricBackBtn" : "reportsMetricBackBtn";
+  const detailEyebrowClass = isHomeMetricDetail ? "homeMetricEyebrow" : "reportsMetricEyebrow";
+  const detailTitleClass = isHomeMetricDetail ? "homeMetricTitle" : "reportsMetricTitle";
+  const detailContextClass = isHomeMetricDetail ? "homeMetricContext" : "reportsMetricContext";
+  const detailHeroWrapClass = isHomeMetricDetail ? "homeMetricHeroWrap" : "reportsMetricHeroWrap";
+  const detailHeroLabelClass = isHomeMetricDetail ? "homeMetricHeroLabel" : "reportsMetricHeroLabel";
+  const detailHeroValueClass = isHomeMetricDetail ? "homeMetricHeroValue" : "reportsMetricHeroValue";
+  const detailCompareClass = isHomeMetricDetail ? "homeMetricCompare" : "reportsMetricCompare";
+  const detailCompareTextClass = isHomeMetricDetail ? "homeMetricCompareText" : "reportsMetricCompareText";
+  const detailCompareRowsClass = isHomeMetricDetail ? "homeMetricCompareRows" : "reportsMetricCompareRows";
+  const detailChartClass = isHomeMetricDetail ? "homeMetricChartBlock" : "reportsMetricChartBlock";
+  const detailChartContextClass = isHomeMetricDetail ? "homeMetricChartContext" : "reportsMetricChartContext";
+  const detailInsightClass = isHomeMetricDetail ? "homeMetricInsight" : "reportsMetricInsight";
 
   const renderNoResultsState = ()=> reportsOverviewSections.renderNoResultsState({
     fMode,
@@ -366,7 +345,21 @@ function renderReportsScreen({ homeMetricOnly = false } = {}){
       rangeLabel,
       trips,
       homeScope: routeContext?.homeScope || null,
-      ...metricDetailClassMap
+      detailSurfaceClass,
+      detailCardClass,
+      detailBackClass,
+      detailEyebrowClass,
+      detailTitleClass,
+      detailContextClass,
+      detailHeroWrapClass,
+      detailHeroLabelClass,
+      detailHeroValueClass,
+      detailCompareClass,
+      detailCompareTextClass,
+      detailCompareRowsClass,
+      detailChartClass,
+      detailChartContextClass,
+      detailInsightClass
     }
     : null;
 
